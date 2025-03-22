6e7b557646cd092f20b94e74ea1c19dae95755d6
mqasim5171 <166934676+mqasim5171@users.noreply.github.com>
2025-03-23 03:32:17 +0500

    Add files via upload

---
+++ ludo/constants.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCM7nVs98RN8UdnVd9PgeRkQNu54c-Z7Es",
+    authDomain: "ludo-e645e.firebaseapp.com",
+    databaseURL: "https://ludo-e645e-default-rtdb.firebaseio.com",
+    projectId: "ludo-e645e",
+    storageBucket: "ludo-e645e.firebasestorage.app",
+     messagingSenderId: "448963096452",
+     appId: "1:448963096452:web:5d8002d3b3baddadca77d1",
+    };
